61c73aa57a2347e0a3e8dfff7157671ed0b182a0
Kafuu Chino <masterspark@openingsource.org>
2019-07-02 15:26:49 +0800

    add decoration about picture

--- main.py
+++ main.py
@@ -1,13 +1,59 @@
 # -*- coding: utf-8 -*-
+import os
+from bs4 import BeautifulSoup
+import urllib.request
+
 def addmodel(name,count):
+
     num=name[0:8]
-    head=open("template/modelhead.html","r",encoding='UTF-8').read()
-    mid=open("template/modelmid.html","r",encoding='UTF-8').read()
-    foot=open("template/modelfoot.html","r",encoding='UTF-8').read()
-    now="<div class=\"row pic-content\" id=\""+str(count)+"\">"+head+"<img class=\"pic\" src="+"pic/"+name+">"+mid+"<a href=\"https://www.pixiv.net/member_illust.php?mode=medium&illust_id="+num+"\">pid="+num+"</a>"+foot
+
+    print("reading page from id "+num)
+    url = 'https://www.pixiv.net/member_illust.php?mode=medium&illust_id=27372812'
+    res = urllib.request.urlopen(url=url)
+    print("reading complete, start reading information")
+
+    soup = BeautifulSoup(res.read(),"lxml")
+
+    title=soup.find('div',class_="userdata").h1.string #标题
+
+    x=soup.find_all('span',class_="views") #浏览与赞
+    visit=x[0].string
+    like=x[1].string
+
+    decoration=(soup.find('div',id="caption_long").string) #图片描述
+
+    userrow=soup.find('div',class_="userdata-row") #用户名 主页 发布日期
+    user=userrow.h2.a.string
+    userpage=userrow.h2.a['href']
+    date=userrow.span.string
+
+    print("reading information complete, start generating html")
+
+    pic=open("template/pic.html","r",encoding='UTF-8').read() #图片模版
+
+    puserdata=open("template/userdata.html","r",encoding='UTF-8').read() #描述模版
+
+    pid=open("template/pid.html","r",encoding='UTF-8').read() #PID模版
+    
+    picHtml=pic.replace('pic-name',name) #html 图片部分
+
+    temp=puserdata.replace('template-date',date) #html 描述部分
+    temp=temp.replace('template-userpage',userpage)
+    temp=temp.replace('template-title',title)
+    temp=temp.replace('template-user',user)
+    temp=temp.replace('template-decoration',decoration)
+    temp=temp.replace('template-like',like)
+    temp=temp.replace('template-visit',visit)
+    puserdataHtml=temp
+
+    pidHtml=pid.replace('pixivid',num) #html pid部分
+
+    print("generating html complete")
+
+    now=picHtml+puserdataHtml+pidHtml
     return now
 
-import os
+print("start reading picture")
 
 number=-1
 fileing=os.walk("pic")
@@ -16,6 +62,8 @@ for root, dirs, files in fileing:
     for fileiii in files:
         number=number+1
         now+=addmodel(files[number],number+1)
+
+print("reading picture complete, start generating PCollection.html")
 
 number=number+1
 midcontrol="<div class=\"text-center mid-control row\" id=\"control\">1/"+str(number)+"</div>"
@@ -31,6 +79,7 @@ result=headhtml+midcontrol+now+"<script>var maxpos="+str(number)+"</script>"+foo
 
 f3=open("PCollection.html","w",encoding='UTF-8')
 f3.write(result)
+print("mission complete")
 
 
 
